d4951ecde99b3ea25a28854c9db404b5dda288a0
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2024-01-15 16:28:39 -0800

    python-hello_world

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -5,22 +5,22 @@
 #include <stdlib.h>
 
 /**
- * struct listint_t] - singly linked list
- * @data: integer
+ * struct listint_s - singly linked list
+ * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- */
+*/
 
-
-typedef struct ListNode
+typedef struct listint_s
 {
-	int data;
-	struct ListNode *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
-int check_cycle(listint_t *list);
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+
 #endif
