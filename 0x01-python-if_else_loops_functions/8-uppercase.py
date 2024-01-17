1cea5c0288d85e38a7f3b14cf22aaea6370af407
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2024-01-17 02:28:19 -0800

    8. To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for m in str:
+        if(65 <= ord(m) <= 122):
+            if(97 <= ord(m) <= 122):
+                m = chr(ord(m) - 32)
+        print("{:s}".format(m), end="")
+    print("")
