863a04098ed2c1cecbc61680f1a90fa2a965de3f
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2024-01-25 05:08:22 -0800

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix):
-      new_matrix = [x[:] for x in matrix]
-      for m in range(len(new_matrix)):
-          for p in range(len(new_matrix[m])):
-              new_matrix[m][p] = (new_matrix[m][p]) ** 2
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
 
+    if len(matrix) > 0:
+        for elems in matrix[:]:
+            new_matrix.append(list(map(lambda x: x ** 2, elems)))
 
-      return new_matrix
+    return new_matrix
