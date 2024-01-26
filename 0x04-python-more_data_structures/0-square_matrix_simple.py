28f3fd4ce7d6cd4be59f11a530de4c37f6588700
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2024-01-26 14:28:14 -0800

    0x04. Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda submat: list(map(lambda e: e**2, submat)), matrix))
+    new_matrix = []
+
+    if len(matrix) > 0:
+        for rows in matrix[:]:
+            new_matrix.append(list(map(lambda x: x ** 2, rows)))
+
+    return new_matrix
