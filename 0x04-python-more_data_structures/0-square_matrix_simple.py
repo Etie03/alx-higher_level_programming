d0ac390987080c4b96133eb91447c150afa5bdbf
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2024-01-26 14:06:33 -0800

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    squared = []
-    for row in matrix:
-        squared.append([m**2 for m in row])
-    return squared
+    return list(map(lambda submat: list(map(lambda e: e**2, submat)), matrix))
