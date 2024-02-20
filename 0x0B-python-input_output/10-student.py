100d1902369022dbfe2f6a4d6160c779eaa1c41b
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2024-02-20 11:25:55 -0800

    Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
-'''Module for Student class.'''
+""" Module that defines the class Student"""
 
 
 class Student:
-    '''Class for jsonification.'''
+    """Class to create student instances """
     def __init__(self, first_name, last_name, age):
-        '''Constructor.'''
+        """method constructor"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        '''Retrieves dictionary with filter.'''
-        if type(attrs) is list and all([type(x) == str for x in attrs]):
-            return {k: v for k, v in self.__dict__.items() if k in attrs}
-        else:
-            return self.__dict__.copy()
+        """Retrieves dictionary"""
+        obj = self.__dict__.copy()
+        if type(attrs) is list:
+
+            for item in attrs:
+                if type(item) is not str:
+                    return obj
+
+            d_list = {}
+
+            for iatr in range(len(attrs)):
+                for satr in obj:
+                    if attrs[iatr] == satr:
+                        d_list[satr] = obj[satr]
+            return d_list
+
+        return obj
