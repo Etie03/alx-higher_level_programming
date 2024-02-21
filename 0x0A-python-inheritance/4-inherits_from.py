4d5d7acd830e283a0defbc2d0a0daaa8c969e19d
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2024-02-21 03:54:24 -0800

    Only sub class of

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,6 +6,4 @@ Validate if a class inherits from a_class
 
 def inherits_from(obj, a_class):
     """Determines if an object is a true subclass of a class"""
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
-        return True
-    return False
+    return isinstance(obj, a_class) and type(obj) is not a_class
