5daec5e5f4d4fede20ce46e1ce4ce99fce0803e8
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2024-03-26 18:49:57 -0700

    Go to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,6 @@
-- Go to UTF8
+-- Script that converts hbtn_0c_0 database to UTF8
+-- Query to convert hbtn_0c_0 database to UTF8
 -- convert db
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- covert table
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
