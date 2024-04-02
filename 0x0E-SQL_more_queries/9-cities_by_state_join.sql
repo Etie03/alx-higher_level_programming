203efb9122d3610b2384ce74846b2d2fa72928b6
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2024-04-02 15:14:59 -0700

    Cities by States

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
- --Script that lists all cities contained in the database
+--Script that lists all cities contained in the database
+-- select join
 SELECT cities.id, cities.name, states.name -- Query to join cities and states
 FROM cities
 JOIN states ON cities.state_id = states.id;
