499d30f6261038581cfb4710a8ec23384ad33861
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2024-04-02 15:53:33 -0700

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,7 @@
 --Script that creates a user and a database in MySQL server
 -- create db
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
+-- create user
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- grant privs
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
