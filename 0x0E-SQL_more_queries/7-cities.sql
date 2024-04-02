0d009b38bea48628d742f16ce88816b97f47c31b
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2024-04-02 14:59:36 -0700

    Cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 - create a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities ( -- Query to create cities in hbtn_0d_usa
+CREATE TABLE IF NOT EXISTS cities ( -- Query to create cities in hbtn_0d_usa
        id INT UNIQUE AUTO_INCREMENT NOT NULL,
        state_id INT NOT NULL,
        name VARCHAR(256) NOT NULL,
