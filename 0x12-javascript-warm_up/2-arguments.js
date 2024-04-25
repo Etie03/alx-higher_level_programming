e924bd4c208b940ef4297a006123e002033a3ca0
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2024-04-24 20:35:22 -0700

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+let m;
+if (process.argv.length < 3) {
+  m = 'No argument';
+} else if (process.argv.length === 3) {
+  m = 'Argument found';
+} else {
+  m = 'Arguments found';
+}
+console.log(m)
