73eb94b2de5ac61dc9f154e1fa1e3e488c8f9852
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2024-05-10 14:11:17 -0700

    post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 """ Fetches header from url passed as arg with email data"""
-from urllib import request, parse
-import sys
 
-if __name__ == "__main__":
-    data = parse.urlencode({"email": sys.argv[2]}).encode()
-    with request.urlopen(sys.argv[1], data) as response:
+
+if __name__ == '__main__':
+    import urllib.request
+    import urllib.parse
+    import sys
+
+    argv = sys.argv
+    url = argv[1]
+    email = argv[2]
+    DATA = urllib.parse.urlencode({"email": email})
+    DATA = DATA.encode('ascii')
+
+    with urllib.request.urlopen(url, DATA) as response:
         print(response.read().decode('utf-8'))
